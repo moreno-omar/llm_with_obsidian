bc2b7a2dfa6b497880b118f6579d2e47f205de3a
Omi <moreno.x7.omar@gmail.com>
2026-06-09 15:28:51 -0700

    put a list of obsidian file names in text. To save time by only doing indexing when file doesn't exist.

--- app/embedding.py
+++ app/embedding.py
@@ -15,17 +15,24 @@ path_to_base_db = Path(base_db)
 
 print(f"{path_to_base_db}")
 
+"""
 # connect to vault. Skip template folder, formatted for plugin, not proper YAML file.
 vault = otools.Vault(path_to_base_db).connect().gather()
 
 # create persistent db
 # w/o path, should default to .chroma file
 client = chromadb.PersistentClient()
+"""
 
-# get list of files and display it
-obsidian_list = vault.md_file_index
+# create file with index of names, if file doesn't exist
+if not os.path.isfile("obsidian_list.txt"):
+    with open("obsidian_list.txt", "wt") as f:
+        obsidian_list = vault.md_file_index
+        for note in obsidian_list:
+            f.write(f"{note}\n")
 
-print(f"{obsidian_list}")
+# get names for files and put them into a list
+with open("obsidian_list.txt", "rt") as f:
+    obsidian_list_of_file_names = f.readlines()
 
-# if __name__ == "__main__":
-#   main()
+print(f"{obsidian_list_of_file_names[5]}")
